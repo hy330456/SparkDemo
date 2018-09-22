82ec6897396cba9185685873faffbf1b923887a9
hy330456 <330456253@qq.com>
2018-09-22 14:52:28 +0800

    first commit

---
+++ src/main/scala/SparkDemo/App.scala
@@ -0,0 +1,9 @@
+package SparkDemo
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/SparkDemo/AppTest.scala
@@ -0,0 +1,17 @@
+package SparkDemo
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
